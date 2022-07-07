99382222d1cb7871d7139e1b9f302213c723fd4e
ohyunsok <hyunsokstar@gmail.com>
2022-07-07 18:33:34 +0900

    update for recent update

--- reservation_app/settings.py
+++ reservation_app/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'qki)d54cmpt_v7b5m07qh$d#o3j6#pm3gi=po&)b2a59$e859x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['158.247.227.197','127.0.0.1', 'www.skilnote-for-ts.co.kr', 'skilnote-for-ts.co.kr']
+ALLOWED_HOSTS = ['158.247.227.197','127.0.0.1', 'www.skilnote-for-react-master.shop', 'skilnote-for-react-master.shop']
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
